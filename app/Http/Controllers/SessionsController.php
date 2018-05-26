92f8dc9025633c325be1bf5ccb97b40c1c554604
ayaup <ayaup@qq.com>
2018-05-26 15:14:27 +0800

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,7 +15,7 @@ class SessionsController extends Controller
             'only' => ['create']
         ]);
     }
-    
+
   public function create()
   {
       return view('sessions.create');
@@ -29,14 +29,21 @@ class SessionsController extends Controller
       ]);
       if (Auth::attempt($credentials,$request->has('remember'))) {
                 // 登录成功后的相关操作
+                 if(Auth::user()->activated) {
                 session()->flash('success', '欢迎回来！');
                return redirect()->intended(route('users.show', [Auth::user()]));
-            } else {
+            } else{
                 // 登录失败后的相关操作
+                  Auth::logout();
+                  session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+             } }else {
                 session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            }
+
             return redirect()->back();
    }
+ }
+
    public function destroy()
        {
            Auth::logout();
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,19 +17,27 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+    protected $hidden = ['password', 'remember_token'];
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
+  
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
+
 }
